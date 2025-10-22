5688e377413037c74376b1c2a4d8910919b8ad21
enterausername55 <120120296+enterausername55@users.noreply.github.com>
2025-10-22 13:30:58 +0200

    Add files via upload

--- models/jobsModel.js
+++ models/jobsModel.js
@@ -8,39 +8,43 @@ export const getJobs = async ({ filter, letter, junior }) => {
     WHERE j.end_date IS NULL
   `;
   const params = [];
+  if (filter) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    let fromDate, toDate;
+    // dátum szűrés
+    switch (filter) {
+      case "today":
+        fromDate = today;
+        toDate = new Date(today);
+        break;
+      case "yesterday":
+        fromDate = new Date(today);
+        fromDate.setDate(today.getDate() - 1);
+        toDate = new Date(today);
+        break;
+      case "last7":
+        fromDate = new Date(today);
+        fromDate.setDate(today.getDate() - 6);
+        break;
+      case "last30":
+        fromDate = new Date(today);
+        fromDate.setDate(today.getDate() - 29);
+        break;
+    }
 
-  // dátum szűrés
-  switch (filter) {
-    case "today":
-      fromDate = today;
-      toDate = new Date(today);
-      break;
-    case "yesterday":
-      fromDate = new Date(today);
-      fromDate.setDate(today.getDate() - 1);
-      toDate = new Date(today);
-      break;
-    case "last7":
-      fromDate = new Date(today);
-      fromDate.setDate(today.getDate() - 6);
-      break;
-    case "last30":
-      fromDate = new Date(today);
-      fromDate.setDate(today.getDate() - 29);
-      break;
-  }
+    const fromDateStr = fromDate.toISOString().split("T")[0];
 
-  const fromDateStr = fromDate.toISOString().split("T")[0];
-
-  if (filter === "yesterday") {
-    const toDateStr = toDate.toISOString().split("T")[0];
-    params.push(fromDateStr, toDateStr);
-    sql += ` AND j.start_date >= $${params.length - 1} AND j.start_date < $${
-      params.length
-    }`;
-  } else {
-    params.push(fromDateStr);
-    sql += ` AND j.start_date >= $${params.length}`;
+    if (filter === "yesterday") {
+      const toDateStr = toDate.toISOString().split("T")[0];
+      params.push(fromDateStr, toDateStr);
+      sql += ` AND j.start_date >= $${params.length - 1} AND j.start_date < $${
+        params.length
+      }`;
+    } else {
+      params.push(fromDateStr);
+      sql += ` AND j.start_date >= $${params.length}`;
+    }
   }
 
   // junior szűrés
